cf99d9a75c4e56af41699d10c7753b5d25479bc6
mndiong <mndion2@students.towson.edu>
2022-05-01 13:41:45 -0400

    Create queries.php

---
+++ queries.php
@@ -0,0 +1,33 @@
+<?php
+
+select *
+from patient
+
+select *
+from medication
+
+select *
+from physician
+
+select *
+from nurse
+
+select *
+from department
+
+select *
+from appointment
+
+select *
+from phlebotomist
+
+select *
+from bloodbank
+
+select *
+from donor
+
+select *
+from bloodTransaction
+
+?>
